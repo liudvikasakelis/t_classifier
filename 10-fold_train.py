01925e77eca62a8b0d5e965e62252adfb2314336
Liudvikas Akelis <liudvikas.akelis@gmail.com>
2018-01-17 17:44:54 +0200

    implemented: parameters in json in command line. Still needs implementing: cutoff

--- 10-fold_train.py
+++ 10-fold_train.py
@@ -13,35 +13,16 @@ from keras.optimizers import Adam
 
 import numpy as np
 import pandas as pd
+import json
 
-config_file = sys.argv[1]
-train_file = sys.argv[2]
-results_file = 'results/' + config_file.split('/')[-1] + str(int(time.time()/60)) + '.results.txt'
-print('Using config file {}, results output to {}'.format(config_file, results_file))
-
-def shuffle_weights(model, weights=None):
-    """Randomly permute the weights in `model`, or the given `weights`.
-    This is a fast approximation of re-initializing the weights of a model.
-    Assumes weights are distributed independently of the dimensions of the weight tensors
-      (i.e., the weights have the same distribution along each dimension).
-    :param Model model: Modify the weights of the given model.
-    :param list(ndarray) weights: The model's weights will be replaced by a random permutation of these weights.
-      If `None`, permute the model's current weights.
-    """
-    if weights is None:
-        weights = model.get_weights()
-    weights = [np.random.permutation(w.flat).reshape(w.shape) for w in weights]
-    # Faster, but less random: only permutes along the first dimension
-    # weights = [np.random.permutation(w) for w in weights]
-    model.set_weights(weights)
 
 def matrixer(line):
     try:
         line = line.lower()
     except AttributeError:
         line = ''
-    l = [ord(x) if ord(x) < alphabet_size else 20 for x in line]
-    l = l + [0] * (l0 - len(l))
+    l = [ord(x) if ord(x) < c['alphabet_size'] else 20 for x in line]
+    l = l + [0] * (c['l0'] - len(l))
     return(l)
 
 def Y_matrixer(Y):
@@ -51,60 +32,41 @@ def Y_matrixer(Y):
     return(ret)
 
 
-configuration = open(config_file).read()
-exec(configuration)
+train_file = sys.argv[1]
+custom_cfg = sys.argv[2]
 
-conv_layers = config.model.conv_layers
-fully_layers = config.model.fully_connected_layers
+default_cfg = {'alpha': 2e-3,
+              'decay': 0,
+              'beta1': 0.9,
+              'beta2': 0.999, 
+              'epochs': 6,
+              'epsilon': 1e-8,
+              'conv_layers': [[256, 7, 3],
+                              [256, 7, 3],
+                              [256, 3, None],
+                              [256, 3, None],
+                              [256, 3, None],
+                              [256, 3, 3]],
+              'fully_connected_layers': [1024, 1024],
+              'th': 1e-6,
+              'embedding_size': 128,
+              'alphabet_size': 400,
+              'l0': 125,
+              'batch_size': 512,
+              'num_of_classes': 112,
+              'dropout_p': 0.5,
+              'train_cutoff': 16750,
+              'CV_cutoff': 16100
+}
 
-l0 = config.l0
-alphabet_size = config.alphabet_size
-num_of_classes = config.num_of_classes
-th = config.model.th
+custom_cfg = json.loads(custom_cfg)
+if (custom_cfg.keys() - default_cfg.keys()):
+    sys.exit('wrong key in config')
 
-embedding_size = config.model.embedding_size
-
-p = config.dropout_p
-cutoff = config.date_cutoff
-
-epsilon = config.training.epsilon
-alpha = config.training.alpha
-beta1 = config.training.beta1
-beta2 = config.training.beta2
-decay = config.training.decay
-
-# Input layer
-inputs = Input(shape=(l0,), name='sent_input', dtype='int64')
-
-# Embedding layer
-
-x = Embedding(alphabet_size + 1, embedding_size, input_length=l0)(inputs)
-
-# Convolution layers
-for cl in conv_layers:
-    x = Convolution1D(cl[0], cl[1])(x)
-    x = ThresholdedReLU(th)(x)
-    if not cl[2] is None:
-        x = MaxPooling1D(cl[2])(x)
-
-x = Flatten()(x)
-
-
-#Fully connected layers
-
-for fl in fully_layers:
-    x = Dense(fl)(x)
-    x = ThresholdedReLU(th)(x)
-    x = Dropout(0.5)(x)
-
-predictions = Dense(num_of_classes, activation='softmax')(x)
-model = Model(input=inputs, output=predictions)
-optimizer = Adam(lr=alpha, beta_1=beta1, beta_2=beta2, epsilon=epsilon,
-                 decay=decay)
-model.compile(optimizer=optimizer, loss='categorical_crossentropy',
-              metrics=['categorical_accuracy'])
-print("New model built")
-
+results_file = 'results/' + str(int(time.time()/60)) + '.results.txt'
+print('Custom config {}, results output to {}'.format(custom_cfg, results_file))
+c = {**default_cfg, **custom_cfg}
+print(c)
 
 print("Loading the data sets...")
 start_time = time.time()
@@ -125,8 +87,8 @@ X = [matrixer(x) for x in fdata['paymentpurpose']]
 Y = fdata.y.tolist()
 
 for i in range(1, len(X)):
-    if len(X[i]) > l0:
-        X[i] = X[i][:l0]
+    if len(X[i]) > c['l0']:
+        X[i] = X[i][:c['l0']]
 
 print("Loadded all data in {}".format(time.time() - start_time))
 print('lengths {} {} (for sanity)'.format(len(X), len(Y)))
@@ -137,8 +99,8 @@ total_eval_results = [['fold', 'epoch', 'categorical_crossentropy',
 for current_fold in range(1, 11):
     print('\nDoing fold {}'.format(current_fold))
     start_time = time.time()
-    test_index = ((fdata.fold == current_fold) & (fdata.operationdate > cutoff))
-    train_index = ((fdata.fold != current_fold) & (fdata.operationdate < cutoff))
+    test_index = ((fdata.fold == current_fold) & (fdata.operationdate > c['CV_cutoff']))
+    train_index = ((fdata.fold != current_fold) & (fdata.operationdate < c['CV_cutoff']))
     X_train = [X[i] for i, value in enumerate(train_index) if value]
     Y_train = [Y[i] for i, value in enumerate(train_index) if value]
     X_test = [X[i] for i, value in enumerate(test_index) if value]
@@ -150,30 +112,35 @@ for current_fold in range(1, 11):
            time.time() - start_time, len(X_train), len(X_test)))
     print('ratio {}'.format(len(X_train)/len(X_test)))
    
-    inputs = Input(shape=(l0,), name='sent_input', dtype='int64')
-    x = Embedding(alphabet_size + 1, embedding_size, input_length=l0)(inputs)
-    for cl in conv_layers:
+    # Input layer
+    inputs = Input(shape=(c['l0'],), name='sent_input', dtype='int64')
+    # Embedding layer
+    x = Embedding(c['alphabet_size'] + 1, c['embedding_size'], 
+                                          input_length=c['l0'])(inputs)
+    # Convolution layers
+    for cl in c['conv_layers']:
         x = Convolution1D(cl[0], cl[1])(x)
-        x = ThresholdedReLU(th)(x)
+        x = ThresholdedReLU(c['th'])(x)
         if not cl[2] is None:
             x = MaxPooling1D(cl[2])(x)
     x = Flatten()(x)
-    for fl in fully_layers:
+    #Fully connected layers
+    for fl in c['fully_connected_layers']:
         x = Dense(fl)(x)
-        x = ThresholdedReLU(th)(x)
+        x = ThresholdedReLU(c['th'])(x)
         x = Dropout(0.5)(x)
-    predictions = Dense(num_of_classes, activation='softmax')(x)
-    model = None
+    predictions = Dense(c['num_of_classes'], activation='softmax')(x)
     model = Model(input=inputs, output=predictions)
-    optimizer = Adam(lr=alpha, beta_1=beta1, beta_2=beta2, epsilon=epsilon,
-                 decay=decay)
+    optimizer = Adam(lr=c['alpha'], beta_1=c['beta1'], beta_2=c['beta2'], 
+                     epsilon=c['epsilon'], decay=c['decay'])
     model.compile(optimizer=optimizer, loss='categorical_crossentropy',
-              metrics=['categorical_accuracy'])
+                  metrics=['categorical_accuracy'])
     print("New model built")
-    for epoch in range(1, config.training.epochs + 1):
-        print('Manual epoch {}/{}'.format(epoch, config.training.epochs))
+
+    for epoch in range(1, c['epochs'] + 1):
+        print('Manual epoch {}/{}'.format(epoch, c['epochs']))
         model.fit(X_train, Y_train, epochs=1, 
-                  batch_size=config.batch_size)
+                  batch_size=c['batch_size'])
         ev_res = model.evaluate(X_test, Y_test, verbose=0)
         ev_res.insert(0, epoch)
         ev_res.insert(0, current_fold) 
