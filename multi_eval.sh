2580fff444a9be2350e782b7d96c3805492d0fb9
Liudvikas Akelis <liudvikas.akelis@gmail.com>
2018-01-22 17:36:40 +0200

    Space age

--- multi_eval.sh
+++ multi_eval.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-for f in night_school/*; do
-	python 10-fold_train.py $f data/transactions/train.data4.csv
+source venv/bin/activate
+
+# get ready
+
+for train in 16436 16801
+do
+	for CV in 16071 16436 
+	do
+		command="{\"train_cutoff\": $train, \"CV_cutoff\": $CV}"
+		python 10-fold_train.py data/transactions/cut.train.data4.csv "$command"
+	done
 done
