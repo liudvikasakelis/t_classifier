3431942ab7a0934b7a5198e0cf63f42b5070e392
Liudvikas Akelis <liudvikas.akelis@gmail.com>
2018-01-23 10:24:11 +0200

    newline in reports added

--- multi_eval.sh
+++ multi_eval.sh
@@ -8,6 +8,6 @@ do
 	for CV in 16071 16436 
 	do
 		command="{\"train_cutoff\": $train, \"CV_cutoff\": $CV}"
-		python 10-fold_train.py data/transactions/cut.train.data4.csv "$command"
+		python 10-fold_train.py data/transactions/train.data4.csv "$command"
 	done
 done
