8153415120fc68c78ab66e28ea82e62356ffab2e
Liudvikas Akelis <liudvikas.akelis@gmail.com>
2018-02-05 14:59:15 +0200

    confusion matrices to results

--- 10-fold_train.py
+++ 10-fold_train.py
@@ -150,15 +150,12 @@ for current_fold in range(1, 11):
         ev_res = model.evaluate(X_test, Y_test, verbose=0)
         
         pred_Y = Y_unmatrixer(model.predict(X_test))
-        conf = confusion_matrix(Y_unmatrixer(Y_test), pred_Y).astype('str')
-        with open('f{}e{}.txt'.format(current_fold, epoch), 'w') as f:
-            for li in conf:
-                f.write(','.join(list(li)))
-                f.write('\n')
+        conf = confusion_matrix(Y_unmatrixer(Y_test), pred_Y)
         ev_res.insert(0, epoch)
         ev_res.insert(0, current_fold) 
         print(ev_res)
         total_eval_results.append(ev_res)
+        total_eval_results.append(np.array2string(conf))
     
     model.save('model{}.test'.format(current_fold))
     print("Done with fold {}!\n".format(current_fold))
